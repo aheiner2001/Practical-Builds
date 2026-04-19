c13efa7d031ab5b6723b443378e10674a903c9b2
Aaron Heiner <65831660+aheiner2001@users.noreply.github.com>
2026-04-18 19:54:58 -0600

    Update checkbox label to include base price

--- streamlit_app/window/upsell_app.py
+++ streamlit_app/window/upsell_app.py
@@ -216,7 +216,7 @@ if upsell_id:
     running_total = base_price
     applied_items = []
 
-    none_op = st.checkbox("Exterior Only", key="none_box")
+    none_op = st.checkbox("Exterior Only: ${:.2f}".format(base_price), key="none_box")
 
     if not none_op:
         st.divider()
